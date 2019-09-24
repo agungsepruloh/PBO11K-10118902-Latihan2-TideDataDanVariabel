cffd1cc3133e10b5dddd9168018313884feb0da2
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-09-24 18:24:52 +0700

    Cara deklarasi dan pengaksesan variabel

---
+++ src/com/pbo/PBO11K10118902Latihan2.java
@@ -0,0 +1,36 @@
+package com.pbo;
+
+/**
+ *
+ * # Author
+ * Nama : Agung Sepruloh
+ * Kelas : PBO11K
+ * NIM : 10118902
+ * Deskripsi Program : Program ini berisi cara deklarasi dan pengaksesan variabel
+ *
+ */
+
+public class PBO11K10118902Latihan2 {
+
+    public static void main(String[] args) {
+
+        // Deklarasi variabel
+        int nilaiInt;
+        final Double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+
+        // Memberi nilai ke variabel
+        nilaiInt = 75;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+    }
+
+}
